86c6b504cacf8fc4d705501edb9ecb9314cb8255
outaagunga <jeconiaagunga@gmail.com>
2023-06-26 13:40:31 +0300

    Completed coding the To Do List

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('#create-task-form'); // Update selector
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(form['new-task-description'].value); // Update to use correct ID
+    form.reset();
+  });
 });
+
+
+function buildToDo (todo) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handDelete);
+  btn.textContent = 'x';
+  p.textContent = `${todo}`;
+  p.appendChild(btn);
+  console.log(p);
+  document.querySelector('#list ul').appendChild(p); // Update selector
+}
+
+
+function handDelete(e) {
+  e.target.parentNode.remove();
+}
